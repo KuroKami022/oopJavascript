c6cd3cc09b40ea0667a78287c1734cf004f0582a
Kami <luisru693@gmail.com>
2023-12-04 07:22:29 -0500

    abstraction / encapsulation / heritage

--- torneo.js
+++ torneo.js
@@ -1,10 +1,3 @@
-let participantes = []
+let participantes = ['0Dany', '1Data', '2Jose', '3Gordo', '4Alirio', '5Bamby', '6Flaco', '7david']
 console.log(Math.round(Math.random() * participantes.length))
 
-//'2Chape' vs '6Dany'
-// '3Data' vs '10Maicol'
-// '11Yoneider' vs '4Jose'
-//'8Kamkum' vs '9Gordo'
-//'0Alirio' vs '7Beto'
-// '5Bamby' vs '12Fransua'
-// '1Flaco' vs '13Fredo'
